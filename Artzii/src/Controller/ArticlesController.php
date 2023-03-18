de327e1b35e005eb02674982b7cbeaa93d97463e
MohamedMokhtarHadded <mohamedmokhtar.hadded@esprit.tn>
2023-03-18 23:16:03 +0100

    FrontEnd Twig

---
+++ Artzii/src/Controller/ArticlesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticlesRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticlesController extends AbstractController
+{
+    #[Route('/articles', name: 'app_articles')]
+    public function goToArticles(ArticlesRepository $rep): Response
+    {
+        $articles = $rep->findAll();
+        return $this->render('article/articles.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+
+    #[Route('/article/ajouter', name: 'app_article_ajouter')]
+    public function goToAjouterArticle(): Response
+    {
+        return $this->render('article/ajouter.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+
+    #[Route('/article/modifier', name: 'app_article_modifier')]
+    public function goToModifierArticle(): Response
+    {
+        return $this->render('article/modifier.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+
+    #[Route('/article/supprimer', name: 'app_article_supprimer')]
+    public function goToSupprimerArticle(): Response
+    {
+        return $this->render('article/supprimer.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+
+}
---
+++ Artzii/src/Entity/Test.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Test
+ *
+ * @ORM\Table(name="test")
+ * @ORM\Entity
+ */
+class Test
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var binary|null
+     *
+     * @ORM\Column(name="mdp_u", type="binary", nullable=true)
+     */
+    private $mdpU;
+
+
+}
